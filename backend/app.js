54f6db9f5835fc9e95adbae39a42a4c47d0dcc00
Gabriel <gabriel.gutz@live.com>
2021-01-22 19:46:41 -0500

    Working on more backend routes, started Validation for Request Body

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,9 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
+//sequelize error importing
+const { ValidationError } = require('sequelize');
+
 //initializing express app
 const app = express();
 
@@ -44,8 +47,40 @@ app.use(
     })
 );
 
+// Connect all the routes
+app.use(routes);
 
-app.use(routes); // Connect all the routes
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error formatter
+//always keep this middleware last in express application
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 //exporting app
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
+//requiring ./api/index.js
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
